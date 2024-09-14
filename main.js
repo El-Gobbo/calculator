9fd3f8ace277d87b8ba90ff124e5903d7e1bb591
El-Gobbo <177414121+El-Gobbo@users.noreply.github.com>
2024-09-14 16:53:05 +0100

    Implement calculate function

--- main.js
+++ main.js
@@ -13,3 +13,26 @@ function multiply(a,b){
 function divide(a,b){
     return (a/b)
 }
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function calculate(firstNumber,secondNumber,operator){
+    let answer;
+    switch (operator){
+        case `add`:
+            answer = add(firstNumber,secondNumber);
+            break;
+        case 'subtract':
+            answer = subtract(firstNumber,secondNumber);
+            break;
+        case `multiply`:
+            answer = multiply(firstNumber,secondNumber);
+            break;
+        case `divide`:
+            answer = divide(firstNumber,secondNumber);
+            break;
+    }
+    return answer;
+}
