0e0cda18cf5dd3e4d9797e870a562705b5206b0c
El-Gobbo <177414121+El-Gobbo@users.noreply.github.com>
2024-09-14 19:50:10 +0100

    Added ability to reset number after calculation

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 function add(a,b){
+    a = Number(a);
+    b = Number(b);
     return (a+b)
 }
 
@@ -16,7 +18,8 @@ function divide(a,b){
 let firstNumber = '';
 let secondNumber = '';
 let currentNumber = '';
-let operator;
+let operator = '';
+let justCalculated = false;
 const cont = document.querySelector('#interface');
 
 cont.addEventListener('click',(e) => {
@@ -24,20 +27,42 @@ cont.addEventListener('click',(e) => {
     let buttonDisplay = e.target.textContent;
     switch(buttonType){
         case ('number'):
+            calculationCheck();
             currentNumber += buttonDisplay;
             updateDisplay(currentNumber);
             break;
         case ('operation'):
             operator = buttonDisplay;
             if (firstNumber !== ''){
-                calculate(firstNumber,currentNumber,operator);
+                saveNumber(calculate(firstNumber,currentNumber,operator));
+                justCalculated = true;
             } else {
-                firstNumber = currentNumber;
-                currentNumber = '';
+                saveNumber(currentNumber); 
             }
             break;
+        default:
+            if (operator !== ''){
+                saveNumber(calculate(firstNumber,currentNumber,operator));
+                justCalculated = true;
+            } else {
+                updateDisplay(currentNumber);
+                currentNumber = ''; 
+            }
     }
 })
+
+function calculationCheck(){
+    if (justCalculated===true){
+        currentNumber = '';
+        firstNumber = '';
+        justCalculated = false;
+    }
+}
+
+function saveNumber(value){
+    firstNumber = value;
+    currentNumber = '';
+}
 
 function updateDisplay(activeNumber){
     let display = document.querySelector('#screen');
@@ -49,16 +74,22 @@ function calculate(firstNumber,secondNumber,operator){
     switch (operator){
         case `+`:
             answer = add(firstNumber,secondNumber);
+            console.log('added')
             break;
         case '-':
             answer = subtract(firstNumber,secondNumber);
+            console.log('subtracted')
             break;
         case `×`:
             answer = multiply(firstNumber,secondNumber);
+            console.log('multiplied')
             break;
         case `÷`:
             answer = divide(firstNumber,secondNumber);
+            console.log('divided')
             break;
+        default:
+            console.log('failed operation');
     }
     updateDisplay(answer);
     return answer;
